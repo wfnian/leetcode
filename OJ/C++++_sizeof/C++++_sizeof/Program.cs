19810faa2156db75a1aa01540f5562854a5cdc15
wfnian <1770547906@qq.com>
2016-06-29 21:52:07 +0800

    C#

---
+++ OJ/C++++_sizeof/C++++_sizeof/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace C____sizeof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", sizeof(int));
+            Console.ReadKey();
+        }
+    }
+}
