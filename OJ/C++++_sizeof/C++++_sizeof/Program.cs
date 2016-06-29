ab16a5be0d997f059834509cc701fb0325a4bc10
wfnian <1770547906@qq.com>
2016-06-29 21:57:38 +0800

    C#1

--- OJ/C++++_sizeof/C++++_sizeof/Program.cs
+++ OJ/C++++_sizeof/C++++_sizeof/Program.cs
@@ -5,8 +5,11 @@ namespace C____sizeof
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}", sizeof(int));
-            Console.ReadKey();
+            double ds = 3784.992;
+            int i = 99;
+            i = (int)ds;
+            Console.WriteLine("{0}", i);
+            Console.ReadLine();
         }
     }
 }
