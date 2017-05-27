444559b7d56712f60fc60742ffd8819357f5bd03
wfnian <1770547906@qq.com>
2017-05-27 10:23:33 +0800

    refer to others answer

--- leetcode/492construct_the_rectangle/492construct_the_rectangle.c
+++ leetcode/492construct_the_rectangle/492construct_the_rectangle.c
@@ -1,10 +1,42 @@
 //492construct_the_rectangle
-#include<stdio.h>
+#include"stdio.h"
 #include<math.h>
+#include<malloc.h>
 int* constructRectangle(int area, int* returnSize) {
-	int n = sqrt(area);
-	printf("%d\n", n);
+	int ans = sqrt(area);
+	while (1)
+	{
+		if (area%ans == 0)
+			break;
+		ans--;
+	}
+	int num[2];
+	num[0] = ans;
+	num[1] = area / ans;
+	*returnSize = 2;
+	return num;
 }
 int main(void) {
-	constructRectangle(14, 2);
+	int  *s;// = (int*)malloc(sizeof(int) * 10);
+	int m;
+	s = constructRectangle(14, &m);
+	printf("%d\n", s[1]);
+	printf("%d\n", m);
 }
+/*Python
+import math
+class Solution(object):
+    def constructRectangle(self, area):
+        """
+        :type area: int
+        :rtype: List[int]
+        """
+        ans = int(math.sqrt(area))
+        while 1:
+            if area % ans == 0:
+                break
+            ans = ans + 1
+        return [area//ans,ans]
+a = Solution()
+print(a.constructRectangle(9999998))
+*/
