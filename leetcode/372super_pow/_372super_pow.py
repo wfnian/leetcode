afa54c3e11e59a97622724bfc535f7d62293a841
wfnian <1770547906@qq.com>
2017-07-12 22:13:17 +0800

    effict

---
+++ leetcode/372super_pow/_372super_pow.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    """def superPow(self, a, b):
+        
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        
+        return pow(a,int(''.join([str(i) for i in b])))%1337"""
+    def superPow(self, a, b):
+        return pow(a, int(''.join(map(str, b))), 1337)
