1b6c275cfca3f19ab727c46345768d7985c79160
wfnian <1770547906@qq.com>
2017-12-01 11:49:35 +0800

    le

---
+++ src/leetcodeIII/_724find_pivot_index.java
@@ -0,0 +1,16 @@
+package leetcodeIII;
+
+public class _724find_pivot_index {
+    public static void main(String []args){
+        int [] nums = {1, 7, 3, 6, 5, 6};
+        Solution solution = new Solution();
+        int i = solution.pivotIndex(nums);
+        System.out.println(i);
+    }
+}
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        return 0;
+    }
+}
