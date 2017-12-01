f153fb6c3bdc65467ecb33f705d0a1185b764f01
wfnian <1770547906@qq.com>
2017-12-01 13:36:31 +0800

    others DP solution

--- src/leetcodeIII/_724find_pivot_index.java
+++ src/leetcodeIII/_724find_pivot_index.java
@@ -2,7 +2,7 @@ package leetcodeIII;
 
 public class _724find_pivot_index {
     public static void main(String []args){
-        int [] nums = {1, 7, 3, 6, 5, 6};
+        int [] nums = {-1,-1,-1,0,1,1};
         Solution solution = new Solution();
         int i = solution.pivotIndex(nums);
         System.out.println(i);
@@ -10,7 +10,32 @@ public class _724find_pivot_index {
 }
 class Solution {
     public int pivotIndex(int[] nums) {
-
-        return 0;
+        for (int i = 0; i < nums.length; i++)
+            if(sum(nums,0,i)==sum(nums,i+1,nums.length))
+                return i;
+        return -1;
+    }
+    public int sum(int [] nums,int i,int j){
+        int _sum=0;
+        for (int k = i; k < j; k++) {
+            _sum += nums[k];
+        }
+        return _sum;
+    }
+}
+class Solution2 {//another solution DP
+    public int pivotIndex(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len + 1];
+        dp[0] = 0;
+        for (int i = 0; i < len;i++) {
+            dp[i+1] = dp[i] + nums[i];
+        }
+        int sum = dp[len];
+        for (int i = 0; i < len; i++) {
+            if (dp[i] == sum - dp[i+1])
+                return i;
+        }
+        return -1;
     }
 }
