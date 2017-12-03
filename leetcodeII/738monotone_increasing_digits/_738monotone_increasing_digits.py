9b41016381452be7fd61f80321929165f1a60707
wfnian <1770547906@qq.com>
2017-12-03 14:51:09 +0800

    add question describe

--- leetcodeII/738monotone_increasing_digits/_738monotone_increasing_digits.py
+++ leetcodeII/738monotone_increasing_digits/_738monotone_increasing_digits.py
@@ -1,3 +1,10 @@
+"""
+Given a non-negative integer N, find the largest number that
+is less than or equal to N with monotone increasing digits.
+
+(Recall that an integer has monotone increasing digits if 
+and only if each pair of adjacent digits x and y satisfy x <= y.)
+"""
 class Solution:
     def monotoneIncreasingDigits(self, N):
         """
