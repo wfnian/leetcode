e5037d72867956af0f7f482ae182c4ec9650ac26
wfnian <1770547906@qq.com>
2018-12-31 14:06:41 +0800

    Create 2018_should_end.cpp
    2018年我和生活打了一架，他死了我也死了。

---
+++ 2018_should_end.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class my_2018
+{
+  private:
+  public:
+    inline my_2018(/* args */)
+    {
+        break;
+    }
+    ~my_2018() {}
+};
+int main(int argc, char const *argv[])
+{
+    my_2018();
+
+    // return 0; // no return;
+}
