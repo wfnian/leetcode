6fa7af22892464343c55fc30cdee61abc0a9f136
wfnian <1770547906@qq.com>
2019-01-06 12:58:36 +0800

    LeetCode Weekly Contest 118,and and i have solved two problems.!! rank:1180~

---
+++ leetcodeII/969pancake_sorting/969pancake_sorting.cpp
@@ -0,0 +1,70 @@
+﻿// 969pancake_sorting.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+/*
+Example 1:
+
+Input: [3,2,4,1]
+Output: [4,2,4,3]
+Explanation:
+We perform 4 pancake flips, with k values 4, 2, 4, and 3.
+Starting state: A = [3, 2, 4, 1]
+After 1st flip (k=4): A = [1, 4, 2, 3]
+After 2nd flip (k=2): A = [4, 1, 2, 3]
+After 3rd flip (k=4): A = [3, 2, 1, 4]
+After 4th flip (k=3): A = [1, 2, 3, 4], which is sorted.
+*/
+class Solution {
+public:
+	vector<int> pancakeSort(vector<int>& A) {
+		int n = A.size();
+		vector<int>ans;
+		int de = 0;
+		vector<int>tmp = A;
+		while (n > 0)
+		{
+			int m_max = tmp[0];
+			int index = 0;
+			for (size_t i = 0; i < n; i++)//找到0-n范围最大值；
+			{
+				if (m_max < tmp[i]) {
+					index = i;
+					m_max = tmp[i];
+				}
+			}
+			//① 反转到最前面
+			reverse(tmp.begin(), tmp.begin() + index + 1);;
+			ans.push_back(index + 1);
+			/*for (auto var : tmp)
+				cout << var << " ";
+			cout << endl;
+			cout << "*" << index + 1 << "*\n";*/
+			//② 整个序列翻转；
+			reverse(tmp.begin(), tmp.end() - de);
+			/*ans.push_back(A.size() - de);
+			for (auto var : tmp)
+				cout << var << " ";
+			cout << endl;
+			cout << "*" << A.size() - de << "*\n";*/
+			//③ n--
+			n--;
+			de++;
+		}
+		return ans;
+	}
+};
+
+int main() {
+
+	vector<int>s = { 3,2,4,1 };
+	Solution().pancakeSort(s);
+
+	return 0;
+}
---
+++ leetcodeII/970powerful_integers/970powerful_integers.cpp
@@ -0,0 +1,52 @@
+﻿// 970powerful_integers.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+/*
+Example 1:
+
+Input: x = 2, y = 3, bound = 10
+Output: [2,3,4,5,7,9,10]
+Explanation:
+2 = 2^0 + 3^0
+3 = 2^1 + 3^0
+4 = 2^0 + 3^1
+5 = 2^1 + 3^1
+7 = 2^2 + 3^1
+9 = 2^3 + 3^0
+10 = 2^0 + 3^2
+*/
+class Solution {
+public:
+	vector<int> powerfulIntegers(int x, int y, int bound) {
+		set<int> res;
+		int p;
+		if (x == y)
+			p = sqrt(x);
+		else p = (log10(bound) / log10(x > y ? x : y)) + 1;
+		for (size_t i = 0; i < p + 2; i++)
+		{
+			for (size_t j = 0; j < p + 2; j++)
+			{
+				int m = pow(x, i) + pow(y, j);
+				if (m <= bound && m > 0)
+					res.insert(m);
+			}
+		}
+
+		return vector<int>(res.begin(), res.end());
+	}
+};
+
+int main() {
+	Solution().powerfulIntegers(90, 90, 1000000);
+
+	return 0;
+}
