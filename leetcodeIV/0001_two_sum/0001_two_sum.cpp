5739e4899feb7259fd31262dfe86fef3ef252e88
wfnian <wfnian@gmail.com>
2020-01-11 22:53:25 +0800

    low efficient

--- leetcodeIV/0001_two_sum/0001_two_sum.cpp
+++ leetcodeIV/0001_two_sum/0001_two_sum.cpp
@@ -3,9 +3,33 @@
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> res;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int deduct = target - nums[i];
+			for (int j = 0; j < nums.size(); j++) {
+				if (j != i && deduct == nums[j])
+				{
+					res.push_back(j);
+				}
+			}
+		}
+		return res;
+	}
+};
 
 int main()
 {
+	vector<int>nums{ 2, 7, 11, 15 };
+	Solution().twoSum(nums, 9);
     std::cout << "Hello World!\n"; 
 }
 
