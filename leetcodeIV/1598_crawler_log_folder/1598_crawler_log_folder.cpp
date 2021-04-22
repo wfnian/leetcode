20ed8999ab3c101e4e32f9f1b0a7ec230b06b63a
wfnian <wfnian@gmail.com>
2021-04-22 21:59:47 +0800

    ...太简单

---
+++ leetcodeIV/1598_crawler_log_folder/1598_crawler_log_folder.cpp
@@ -0,0 +1,43 @@
+﻿// 1598_crawler_log_folder.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int res = 0;
+        for (auto &i : logs)
+        {
+            if (i.compare("../") == 0)
+            {
+                if (res != 0)
+                    res--;
+            }
+            else if (i.compare("./") == 0)
+                ;
+            else
+                res++;
+            
+        }
+        return res;
+    }
+};
+
+
+
+int main()
+{
+    vector<string> str = { "d1/","d2/","../","d21/","./" };
+
+    Solution().minOperations(str);
+    vector<string> str1 = { "d1/","../","../","../" };
+    Solution().minOperations(str1);
+
+    return 0;
+}
+
